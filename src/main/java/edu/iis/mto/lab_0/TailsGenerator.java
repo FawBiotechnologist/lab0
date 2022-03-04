a16054e38b6c4bfe5e179e1f4fc6d1ac1c653aa3
lesniewski.karol <222491@edu.p.lodz.pl>
2022-03-04 17:42:42 +0100

    przechodzi wszystkie testy

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,13 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+        int valueSize = value.length();
+        ArrayList<String> list = new ArrayList<>(value.length());
+        for(int i = 0; i <= valueSize; i++){
+            String s = value.substring(i, valueSize);
+            list.add(s);
+        }
+        return list;
     }
 
 }
