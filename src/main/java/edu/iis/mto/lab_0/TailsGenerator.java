77785f58e1b01e69a5961c347c4cbe0a07b6861b
Lukasz Krulik <228780@edu.p.lodz.pl>
2022-03-10 15:45:22 +0100

    Tails easy solution and fun one

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -2,12 +2,26 @@ package edu.iis.mto.lab_0;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class TailsGenerator {
+    //easy solution
 
+    public List<String> tailsEasySolution(String value) {
+        ArrayList<String> wordsContainer = new ArrayList<>(value.length() + 1);
+        for (int i = 0; i < value.length() + 1; i++) {
+            wordsContainer.add(value.substring(i));
+        }
+        return wordsContainer;
+    }
+
+    //fun with Streams
     public List<String> tails(String value) {
-
-        return new ArrayList<>(value.length());
+        return IntStream
+                .range(0, value.length() + 1)
+                .mapToObj(value::substring)
+                .collect(Collectors.toList());
     }
 
 }
